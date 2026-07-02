ea1fe5fce8c9824fd5b19777686fd28be7465653
ananddigitalloom2521 <ananddigitalloom@gmail.com>
2026-07-02 17:13:11 +0530

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const routes = require('./routes/index');
+app.use('/api', routes);
+app.listen(port, () => console.log(`Server started on port ${port}`));
